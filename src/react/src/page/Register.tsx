6df04b0c3ee683ff799f9a2165be48d4d5616769
Võ Nguyên Gia Bảo <giabao@GB.lan>
2023-06-17 20:26:39 +0700

    [w1][Bao]enhance register api

--- src/react/src/page/Register.tsx
+++ src/react/src/page/Register.tsx
@@ -11,51 +11,80 @@ const onFinish = (values: any) => {
     console.log('Failed:', errorInfo);
   };
 
-const Register: React.FC = () => (
+const Register: React.FC = () => {
 
-    <form className='center1'>
-        <h1>REGISTER ACCOUNT</h1>
-        <Form 
-        name="basic"
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 16 }}
-        style={{ maxWidth: 600 }}
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-        >
-        <Form.Item label="First name" name="first name" rules={[{ required: true, message: 'Please input your first name!' }]}>
-            <Input />
-        </Form.Item>
+    const [first_name, setFirst_Name] = useState('');
+    const [last_name, setLast_Name] = useState('');
+    const [role, setRole] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [password, setPassword] = useState('');
 
-        <Form.Item label="Last name" name="last name" rules={[{ required: true, message: 'Please input your last name!' }]}>
-            <Input />
-        </Form.Item>
+    const submitbtw = async (e: SyntheticEvent) => {
+        e.preventDefault();
 
-        <Form.Item label="Email" name="email" rules={[{ required: true, message: 'Please input your email!' }]}>
-            <Input />
-        </Form.Item>
+        await fetch('http://localhost:3000/api/register', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                "first_name" : first_name,
+                "last_name" : last_name,
+                "role" : role,
+                "email" : email,
+                "password" : password,
+                "phone" : phone
+            })
+        });
+    }
+    return (
+        <form className='center1'>
+            <h1>REGISTER ACCOUNT</h1>
+            <Form 
+            name="basic"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}
+            style={{ maxWidth: 600 }}
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+            >
 
-        <Form.Item label="Phone" name="phone" rules={[{ required: true, message: 'Please input your phone!' }]}>
-            <Input />
-        </Form.Item>
+            <Form.Item label="First name" name="first name" rules={[{ required: true, message: 'Please input your first name!' }]}>
+                <Input onChange={e => setFirst_Name(e.target.value)} />
+            </Form.Item>
 
-        <Form.Item label="Password" name="password" rules={[{ required: true, message: 'Please input your password!' }]}>
-            <Input />
-        </Form.Item>
-    
-        <Form.Item label="Confirm" name="confirm" rules={[{ required: true, message: 'Please confirm your password!' }]}>
-            <Input.Password />
-        </Form.Item>
+            <Form.Item label="Last name" name="last name" rules={[{ required: true, message: 'Please input your last name!' }]}>
+                <Input onChange={e => setLast_Name(e.target.value)} />
+            </Form.Item>
 
-        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-            <Button type="primary" htmlType="submit">
-            Submit
-            </Button>
-        </Form.Item>
-        </Form>
-    </form>
-);
+            <Form.Item label="Role" name="role" rules={[{ required: true, message: 'Please input your role!' }]}>
+                <Input onChange={e => setRole(e.target.value)} />
+            </Form.Item>
+
+            <Form.Item label="Email" name="email" rules={[{ required: true, message: 'Please input your email!' }]}>
+                <Input onChange={e => setEmail(e.target.value)} />
+            </Form.Item>
+
+            <Form.Item label="Phone" name="phone" rules={[{ required: true, message: 'Please input your phone!' }]}>
+                <Input onChange={e => setPhone(e.target.value)} />
+            </Form.Item>
+
+            <Form.Item label="Password" name="password" rules={[{ required: true, message: 'Please input your password!' }]}>
+                <Input onChange={e => setPassword(e.target.value)} />
+            </Form.Item>
+        
+            <Form.Item label="Confirm" name="confirm" rules={[{ required: true, message: 'Please confirm your password!' }]}>
+                <Input.Password />
+            </Form.Item>
+            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                <Button type="primary" htmlType="submit" onClick={() => submitbtw}>
+                    Submit
+                </Button>
+            </Form.Item>
+            </Form>
+        </form>
+    );
+};
 
 export default Register;
