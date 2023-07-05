25e20ea5d42de0e35c33128a36c03b043c60a803
Lô Thuỷ Tiên <92684842+Locung201@users.noreply.github.com>
2023-07-05 09:23:03 +0700

    [s2]Login design

--- src/react/src/page/LogIn.tsx
+++ src/react/src/page/LogIn.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import './LogIn.css';
 import { Button, Checkbox, Form, Input } from 'antd';
+
 
 const onFinish = (values: any) => {
   console.log('Success:', values);
@@ -9,43 +11,49 @@ const onFinishFailed = (errorInfo: any) => {
   console.log('Failed:', errorInfo);
 };
 
-const LogIn: React.FC = () => (
-  <Form
-    name="basic"
-    labelCol={{ span: 8 }}
-    wrapperCol={{ span: 16 }}
-    style={{ maxWidth: 600 }}
-    initialValues={{ remember: true }}
-    onFinish={onFinish}
-    onFinishFailed={onFinishFailed}
-    autoComplete="off"
-  >
-    <Form.Item
-      label="Username"
-      name="username"
-      rules={[{ required: true, message: 'Please input your username!' }]}
-    >
-      <Input />
-    </Form.Item>
+const LogIn: React.FC = () => {
+  return (
+    <div className='Box-Login'>
+      <div className='Login'>
+        <h1>LOGIN</h1>
+        <Form
+          name="basic"
+          labelCol={{ span: 6 }}
+          wrapperCol={{ span: 16 }}
+          style={{ maxWidth: 600 }}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+          <Form.Item
+            label="Username"
+            name="username"
+            rules={[{ required: true, message: 'Please input your username!' }]}
+          >
+            <Input />
+          </Form.Item>
 
-    <Form.Item
-      label="Password"
-      name="password"
-      rules={[{ required: true, message: 'Please input your password!' }]}
-    >
-      <Input.Password />
-    </Form.Item>
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: 'Please input your password!' }]}
+          >
+            <Input.Password />
+          </Form.Item>
 
-    <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
-      <Checkbox>Remember me</Checkbox>
-    </Form.Item>
+          <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 4, span: 16 }}>
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
 
-    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-      <Button type="primary" htmlType="submit">
-        Submit
-      </Button>
-    </Form.Item>
-  </Form>
-);
-
+          <Form.Item wrapperCol={{ offset: 4, span: 16 }}>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  );
+};
 export default LogIn;
--- src/react/src/page/Register.tsx
+++ src/react/src/page/Register.tsx
@@ -1,22 +1,22 @@
-import React, {SyntheticEvent, useState} from 'react';
+import React, { SyntheticEvent, useState } from 'react';
 import './Register.css';
 import { Button, Form, Input } from 'antd';
 
-const onFinish = (data : any) => {
+const onFinish = (data: any) => {
     //console.log("truoc khi xu ly: ", data)
     const jsonData = JSON.stringify(data)
     //console.log("sau khi xu ly: ", jsonData)
     fetch('http://localhost:3000/api/register', {
         method: 'POST',
-        headers: {'Content-Type': 'application/json'},
+        headers: { 'Content-Type': 'application/json' },
         body: jsonData
     })
     //console.log('Success:', data);
-  };
-  
-  const onFinishFailed = (errorInfo: any) => {
+};
+
+const onFinishFailed = (errorInfo: any) => {
     console.log('Failed:', errorInfo);
-  };
+};
 
 const Register: React.FC = () => {
 
@@ -27,7 +27,7 @@ const Register: React.FC = () => {
     const [phone, setPhone] = useState('');
     const [password, setPassword] = useState('');
 
-    
+
 
     // const submitbtw = async (e: SyntheticEvent) => {
     //     e.preventDefault();
@@ -40,56 +40,56 @@ const Register: React.FC = () => {
     //         password: password,
     //         phone: phone
     //     };
-        
+
     //     //const jsonData = JSON.stringify(data)
     //     onFinish(data)
     // }
     return (
         <div className='center1'>
             <h1>REGISTER ACCOUNT</h1>
-            <Form 
-            name="basic"
-            labelCol={{ span: 8 }}
-            wrapperCol={{ span: 16 }}
-            style={{ maxWidth: 600 }}
-            initialValues={{ remember: true }}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-            autoComplete="off"
+            <Form
+                name="basic"
+                labelCol={{ span: 8 }}
+                wrapperCol={{ span: 16 }}
+                style={{ maxWidth: 600 }}
+                initialValues={{ remember: true }}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                autoComplete="off"
             >
 
-            <Form.Item label="First name" name="FirstName" rules={[{ required: true, message: 'Please input your first name!' }]}>
-                <Input onChange={e => setFirstName(e.target.value)} />
-            </Form.Item>
+                <Form.Item label="First name" name="FirstName" rules={[{ required: true, message: 'Please input your first name!' }]}>
+                    <Input onChange={e => setFirstName(e.target.value)} />
+                </Form.Item>
 
-            <Form.Item label="Last name" name="LastName" rules={[{ required: true, message: 'Please input your last name!' }]}>
-                <Input onChange={e => setLastName(e.target.value)} />
-            </Form.Item>
+                <Form.Item label="Last name" name="LastName" rules={[{ required: true, message: 'Please input your last name!' }]}>
+                    <Input onChange={e => setLastName(e.target.value)} />
+                </Form.Item>
 
-            <Form.Item label="Role" name="Role" rules={[{ required: true, message: 'Please input your role!' }]}>
-                <Input onChange={e => setRole(e.target.value)} />
-            </Form.Item>
+                <Form.Item label="Role" name="Role" rules={[{ required: true, message: 'Please input your role!' }]}>
+                    <Input onChange={e => setRole(e.target.value)} />
+                </Form.Item>
 
-            <Form.Item label="Email" name="Email" rules={[{ required: true, message: 'Please input your email!' }]}>
-                <Input onChange={e => setEmail(e.target.value)} />
-            </Form.Item>
+                <Form.Item label="Email" name="Email" rules={[{ required: true, message: 'Please input your email!' }]}>
+                    <Input onChange={e => setEmail(e.target.value)} />
+                </Form.Item>
 
-            <Form.Item label="Phone" name="Phone" rules={[{ required: true, message: 'Please input your phone!' }]}>
-                <Input onChange={e => setPhone(e.target.value)} />
-            </Form.Item>
+                <Form.Item label="Phone" name="Phone" rules={[{ required: true, message: 'Please input your phone!' }]}>
+                    <Input onChange={e => setPhone(e.target.value)} />
+                </Form.Item>
 
-            <Form.Item label="Password" name="Password" rules={[{ required: true, message: 'Please input your password!' }]}>
-                <Input onChange={e => setPassword(e.target.value)} />
-            </Form.Item>
-        
-            <Form.Item label="Confirm" name="confirm" rules={[{ required: true, message: 'Please confirm your password!' }]}>
-                <Input.Password />
-            </Form.Item>
-            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-                <Button type="primary" htmlType="submit">
-                    Submit
-                </Button>
-            </Form.Item>
+                <Form.Item label="Password" name="Password" rules={[{ required: true, message: 'Please input your password!' }]}>
+                    <Input onChange={e => setPassword(e.target.value)} />
+                </Form.Item>
+
+                <Form.Item label="Confirm" name="confirm" rules={[{ required: true, message: 'Please confirm your password!' }]}>
+                    <Input.Password />
+                </Form.Item>
+                <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                    <Button type="primary" htmlType="submit">
+                        Submit
+                    </Button>
+                </Form.Item>
             </Form>
         </div>
     );
