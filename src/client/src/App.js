c6ac3039c3c2670a114bbbe86438ebef93d8d3de
Lô Thuỷ Tiên <92684842+Locung201@users.noreply.github.com>
2023-08-19 20:45:17 +0700

    Add Post Widget
    Add Post Widget

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import LoginPage from "scenes/loginPage";
 import HomePage from "scenes/homePage";
 import React from 'react';
-import ProfilePage from "scenes/profilePage";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { CssBaseline, ThemeProvider } from "@mui/material";
@@ -24,10 +23,6 @@ function App() {
             <Route
               path="/home"
               element={isAuth ? <HomePage /> : <Navigate to="/" />}
-            />
-            <Route
-              path="/profile/:userId"
-              element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
             />
           </Routes>
         </ThemeProvider>
