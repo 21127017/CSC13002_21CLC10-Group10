fb7f942de6db32b5f49efff842cef7de824357ba
trieuan151203 <lstan21@clc.fitus.edu.vn>
2023-08-23 13:47:19 +0700

    Pending withdraw request + admin receive pending request

--- src/server/controllers/users.js
+++ src/server/controllers/users.js
@@ -174,3 +174,72 @@ export const denyDepositRequest = async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 };
+////////////////
+export const addWithdrawRequest = async (req, res) => {
+  try {
+    const { id } = req.params; 
+    const { amount } = req.body;
+    console.log(id)
+    const user = await User.findById(id);
+    // Thêm request vào mảng depositRequests của người dùng
+    if (!user) {
+      return res.status(404).json({ message: "User not found." });
+    }
+    //const { amount } = req.body;
+    const depositAmount = parseFloat(amount);
+    if (isNaN(depositAmount)) {
+      return res.status(400).json({ message: "Invalid amount." });
+    }
+    const request = {
+      type_: "withdraw", // Loại giao dịch: deposit
+      date_: new Date(), // Ngày giao dịch
+      amount: depositAmount,
+      status: "pending", // Trạng thái: pending
+    };
+    console.log(request)
+    enqueueDepositRequest(request);
+    if (!user.depositRequests) {
+      user.depositRequests = [];
+    }
+    user.depositRequests.push(request);
+    await user.save();
+    res.status(200).json({ message: "Deposit request added to queue." });
+  } catch (err) {
+    console.error('Error adding deposit request:', err);
+    res.status(500).json({ message: err.message });
+  }
+};
+
+// ... trong một hàm xử lý cho admin
+export const getAllPendingDepositRequests = async (req, res) => {
+  try {
+    const users = await User.find();
+
+    const pendingDepositRequests = users.reduce((acc, user) => {
+      const pendingRequests = user.depositRequests.filter(
+        (request) => request.status === "pending"
+      );
+
+      pendingRequests.forEach((request) => {
+        acc.push({
+          email: user.email,
+          userid: user._id,
+          amount: request.amount,
+          date_: request.date_,
+          type_: request.type_,
+          postid: request._id, // Thay 'postid' bằng tên thực tế của trường trong DepositRequestSchema
+        });
+      });
+
+      return acc;
+    }, []);
+
+    console.log(pendingDepositRequests);
+    res.status(200).json(pendingDepositRequests);
+  } catch (err) {
+    console.error("Error getting all pending deposit requests:", err);
+    res.status(500).json({ message: err.message });
+  }
+};
+
+
--- src/server/routes/users.js
+++ src/server/routes/users.js
@@ -4,8 +4,10 @@ import {
   getUserFriends,
   addRemoveFriend,
   addDepositRequest,
+  addWithdrawRequest,
   denyDepositRequest,
   acceptDepositRequest,
+  getAllPendingDepositRequests,
 } from "../controllers/users.js";
 import { verifyToken } from "../middleware/auth.js";
 const router = express.Router();
@@ -18,6 +20,8 @@ router.get("/:id/friends", verifyToken, getUserFriends);
 router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
 
 router.post("/:id/deposit", verifyToken, addDepositRequest);
+router.post("/:id/withdraw", verifyToken, addWithdrawRequest);
+router.get("/:id/getpending", verifyToken, getAllPendingDepositRequests);
 router.get("/:id/:id_transaction/accept", verifyToken, acceptDepositRequest);
 router.get("/:id/:id_transaction/deny", verifyToken, denyDepositRequest);
 
